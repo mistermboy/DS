7f6e689af2c13102f26f15228b6b93802595a0ee
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-01-15 16:09:58 +0100

    P1

---
+++ Práctica 1/Videoclub/Videoclub (Before)/src/Rental.java
@@ -0,0 +1,24 @@
+/**
+ * Esta clase representa el alquiler de una película por parte de un cliente.
+ */
+public class Rental 
+{
+	private Movie movie;
+	private int daysRented;
+	
+	public Rental(Movie movie, int daysRented) 
+	{
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() 
+	{
+		return daysRented;
+	}
+
+	public Movie getMovie() 
+	{
+		return movie;
+	}
+}
