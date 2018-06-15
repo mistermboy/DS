e0283f7c552326f9762b9515b5c1206f9e0d313b
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2018-06-15 18:39:18 +0200

    P1 before

--- Práctica 1/Videoclub/Videoclub (Before)/src/Customer.java
+++ Práctica 1/Videoclub/Videoclub (Before)/src/Customer.java
@@ -1,44 +1,67 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-public class Customer {
+public class Customer 
+{
 	private String name;
 	private List<Rental> rentals = new ArrayList<Rental>();
-
-	public Customer(String name) {
+	
+	public Customer(String name) 
+	{
 		this.name = name;
 	}
-
-	public void addRental(Rental rental) {
+	
+	public void addRental(Rental rental) 
+	{
 		rentals.add(rental);
 	}
-
-	public String getName() {
+	
+	public String getName() 
+	{
 		return name;
 	}
-
-	public String statement() {
+	
+	public String statement() 
+	{
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-
+		
 		for (Rental each : rentals) {
 			double thisAmount = 0;
-			thisAmount += each.getPrice();
-
+			
+			// Calcula el importe de cada alquiler
+			switch (each.getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2) 
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+			}
+			
 			// Añade los puntos de alquiler frecuente
 			frequentRenterPoints++;
-			frequentRenterPoints += each.getFrequentRenterPoints();
-
+			// Un punto extra en el caso de las novedades alquiladas por un período de dos o más días
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+				frequentRenterPoints++;
+			
 			// Muestra el importe de esta película alquilada
-			result += "\t" + each.getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
-
+		
 		// Añade las líneas de total
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-
+		
 		return result;
 	}
 }
