8ecd53987b6c6efb50af6612cc6f1eaf00547fa9
Pablo Menéndez Suárez <UO252406@uniovi.es>
2018-10-01 10:52:59 +0200

    added statements

---
+++ Aquilino/Practica3 (Enunciado)/src/editor/Dibujo.java
@@ -0,0 +1,11 @@
+package editor;
+
+import java.util.*;
+
+public class Dibujo {
+
+	public void dibujar() {
+		System.out.println("Estoy vacio (por ahora)");
+	}
+
+}
---
+++ Aquilino/Practica3 (Enunciado)/src/editor/Editor.java
@@ -0,0 +1,22 @@
+package editor;
+
+public class Editor {
+
+	public Editor(Dibujo dibujo) {
+		setDibujo(dibujo);
+	}
+
+	public void setDibujo(Dibujo dibujo) {
+		this.dibujo = dibujo;
+	}
+
+	public Dibujo getDibujo() {
+		return dibujo;
+	}
+
+	private Dibujo dibujo;
+
+	public void dibujar() {
+		dibujo.dibujar();
+	}
+}
